ff32edb7d0758b8d8e38dece5e5f25e794355193
Hunter Crisp <hcrisp43@gmail.com>
2023-10-04 10:01:12 -0500

    Simple Calculator
    First project

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿int firstInput, secondInput;
+string userChoice;
+
+Console.WriteLine("Please enter the first number: ");
+firstInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number; ");
+secondInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine("What do you want to do?\n [A]dd\n [S]ubtract\n [M]ultiply");
+userChoice = Console.ReadLine();
+
+calculate(userChoice);
+
+Console.WriteLine("Press any key to close");
+Console.ReadKey();
+
+void calculate(string choice)
+{
+    if(choice == "a" || choice == "A")
+    {
+        Console.WriteLine(firstInput + " " + secondInput + " = " + (firstInput + secondInput));
+    }
+    else if(choice == "s" || choice == "S")
+    {
+        Console.WriteLine(firstInput + " - " + secondInput + " = " + (firstInput - secondInput));
+    }
+    else if(choice == "m" || choice == "M")
+    {
+        Console.WriteLine(firstInput + " * " + secondInput + " = " + (firstInput * secondInput));
+    }
+    else
+    {
+        Console.WriteLine("Invalid option");
+    }
+}
